c8ab3b2646ea5deab67e0ca0c9b61de813a30a36
unknown <jaguarvillalobos@gmail.com>
2021-10-09 11:52:06 -0400

    start on talon for Archie

---
+++ src/main/java/frc/libraries/TalonTest.java
@@ -0,0 +1,11 @@
+import frc.libraries.*;
+import frc.libraries.TalonSRX;
+import frc.robot.TalonSRX1038;
+import frc.libraries.Joystick1038;
+// Talon motor ports 
+private final int Talon_Motor_Port = 65;
+// Initialize the talon
+private TalonSRX1038 Motor1 = new TalonSRX1038(Talon_Motor_Port);
+public void talonMoveLeft() {
+
+}
